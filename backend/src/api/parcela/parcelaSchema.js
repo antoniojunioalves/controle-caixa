20ec74cf05f310f6606cf01430c952ac9d2c5507
antoniojunioalves <antoniojunioalves@gmail.com>
2019-07-05 18:04:21 -0300

    Adicionando parcelas

--- backend/src/api/parcela/parcelaSchema.js
+++ backend/src/api/parcela/parcelaSchema.js
@@ -2,8 +2,8 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const parcelaSchema = new mongoose.Schema({
-    descricao: { type: String, require: true },
-    nroParcela: { type: Number, require: true  },
+    descricao: { type: String, required: true },
+    nroParcela: { type: Number, required: true  },
     valor: { type: Number, default: 0 },
     pago: { type: Boolean, default: false },
     
@@ -11,7 +11,7 @@ const parcelaSchema = new mongoose.Schema({
 })
 
 module.exports = restful.model('Parcela', parcelaSchema)
-
+module.exports.parcelaSchema = parcelaSchema
 // descricao: Mesa TokStok
 // nroParcela: 1
 // valor: 99,9
