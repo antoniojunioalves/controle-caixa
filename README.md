053457d26a09cfdae117a5161122dd0d6d899d79
antoniojunioalves <antoniojunioalves@gmail.com>
2019-07-08 17:00:18 -0300

    Alterações:  -> Exclusão da parcela e acomplando ela dentro do título.  -> Adicionando um arquivo XLS como exemplo do arquivo que vou querer gerar futuramente na aplicação para suprir minha necessidade.  -> Adicionando algumas informações no arquivo README.md.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# controle-caixa
+# Controle de caixa
+
+### `Como executar`
+Back-end
+```bash
+cd backend
+npm install
+npm run dev
+```
+o backend ficará disponível na Url [http://localhost:3003](http://localhost:3003) 
+
+Front-end
+```bash
+ Em construção
+```
--- backend/src/api/parcela/parcelaService.js
+++
@@ -1,9 +0,0 @@
-const Parcela = require('./parcelaSchema')
-
-Parcela.methods(['get', 'post', 'put', 'delete'])
-
-// new - Quando altera o cliente NÃO retorna o objeto atualizado por default 
-// runValidators - Não valida por default as configurações se é obrigado ou não informar alguma informação 
-Parcela.updateOptions({ new: true, runValidators: true }) 
-
-module.exports = Parcela
