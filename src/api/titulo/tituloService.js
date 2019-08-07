5cb4a504622b01249f73289954dd7ab6a779ed38
antoniojunioalves <antoniojunioalves@gmail.com>
2019-08-07 00:09:57 -0300

    Feito para adicionar mais de um mês no retorno para o front-end - teste beta

--- src/api/titulo/tituloService.js
+++ src/api/titulo/tituloService.js
@@ -9,14 +9,57 @@ Titulo.methods(['get', 'post', 'put', 'delete'])
 
 Titulo.route('separadoMes', (req, res, next) => {
   const q = url.parse(req.url, true)
-  const nMonths = q.query.month.split('-').sort()
-  console.log(nMonths)
 
+  const nMonths = [6, 7] // q.query.month.split('-').sort()
+
+  const months = []
   Titulo.find({ "parcelas.mes": { $in: nMonths } }, function (err, docs) {
+    for (let i = 0; i < nMonths.length; i++) {
+      const parcelasMes = docs.map((doc) => {
+        console.log('****************   DOC  ****************')
+        console.log(doc)
+        const encontrou = doc.parcelas.find(({ mes }) => mes === nMonths[i])
+        if (encontrou) {
+          const adicionarRegistrosTemp = {
+            descricao: doc.descricao + nMonths[i],
+            tipoLancamento: doc.tipoLancamento,
+            valor: encontrou.valor,
+            pago: encontrou.pago
+          }
+          return adicionarRegistrosTemp
+        } else {
+          return null
+          // console.log('Não Encontrou ')
+          // const adicionarRegistrosTemp = {
+          //   descricao: 'NAOTRAZER',
+          //   tipoLancamento: '1',
+          //   valor: 1,
+          //   pago: false
+          // }
+          // return adicionarRegistrosTemp
+        }
+      })
+      console.log(`****************   MES ${nMonths[i]}   **************************`)
+      console.log(parcelasMes)
 
+      const parcelasSemNull = parcelasMes.filter(parcela => parcela !== null)
+      console.log(`****************   parcela sem null   **************************`)
+      console.log(parcelasSemNull)
+      const antonio = {
+        mes: nMonths[i],
+        parcelasSemNull
+      }
+      console.log(`****************   ANTONIO ${nMonths[i]}   **************************`)
+      console.log(antonio)
+      // months = [...months, antonio]
+      console.log(`****************   PASSSSSOOOOUUUUUUUUUU   **************************`)
+      // console.log('PARCELASSSSSSSS')
+      // console.log(parcelasMes)
+      // console.log('resultado FINAL:')
 
-
-    res.status(500).json({ docs })
+      console.log({ months })
+    }
+    res.status(500).json(months)
 
   }).sort({ "descricao": 1 })
 
